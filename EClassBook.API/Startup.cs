2b84a18dad80b9dbfc03b332671c7427fcdaac62
VenelinGP <venelin.petkov@netcoms.eu>
2016-10-28 23:41:42 +0300

    Changed Startup.cs

--- EClassBook.API/Startup.cs
+++ EClassBook.API/Startup.cs
@@ -13,51 +13,96 @@
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using Model.Repositories;
+    using Common.Models;
+    using Data.Repositories;
+    using Data.Contracts;
+    using Data.Service;
+    using System.Security.Claims;
+    using Newtonsoft.Json.Serialization;
+    using Microsoft.Extensions.FileProviders;
 
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        private static string applicationPath = string.Empty;
+        private static string contentRootPath = string.Empty;
+
         public Startup(IHostingEnvironment env)
         {
+            applicationPath = env.WebRootPath;
+            contentRootPath = env.ContentRootPath;
             var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
+                .SetBasePath(contentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
             if (env.IsDevelopment())
             {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                // This will push telemetry data through Application Insights pipeline faster,
+                // allowing you to view results immediately.
                 builder.AddApplicationInsightsSettings(developerMode: true);
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
             }
-
+            builder.AddEnvironmentVariables();
             this.Configuration = builder.Build();
         }
 
-        public IContainer ApplicationContainer { get; private set; }
+        //public IContainer ApplicationContainer { get; private set; }
 
         public IConfigurationRoot Configuration { get; set; }
 
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddDbContext<EBookContext>(options => options
                 .UseSqlServer(this.Configuration.GetConnectionString("EClassBookDatabase"),
                 b => b.MigrationsAssembly("EClassBook.Data")));
 
-            // Create the container builder.
-            var builder = new ContainerBuilder();
-            //builder.RegisterModule(new AutofacModule());
+            //services.AddDbContext<EBookContext>(options => options
+            //    .UseSqlServer(Configuration["Data:PhotoGalleryConnection:ConnectionString"]));
 
-            //builder.RegisterType<MyType>().As<IMyType>();
-            builder.RegisterGeneric(typeof(EntityBaseRepository<>))
-                .As(typeof(IEntityBaseRepository<>))
-                .InstancePerRequest();
-            builder.Populate(services);
-            this.ApplicationContainer = builder.Build();
+            // Repositories
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<IGradeRepository, GradeRepository>();
+            services.AddScoped<IHeadmasterRepository, HeadmasterRepository>();
+            services.AddScoped<ITeacherRepository, TeacherRepository>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<ILoggingRepository, LoggingRepository>();
 
-            return new AutofacServiceProvider(this.ApplicationContainer);
+            // Services
+            services.AddScoped<IMembershipService, MembershipService>();
+            services.AddScoped<IEncryptionService, EncryptionService>();
+
+            services.AddAuthentication();
+
+            // Polices
+            services.AddAuthorization(options =>
+            {
+                // inline policies
+                options.AddPolicy("AdminOnly", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "Admin");
+                });
+
+            });
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+            .AddJsonOptions(opt =>
+            {
+                var resolver = opt.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                {
+                    var res = resolver as DefaultContractResolver;
+                    res.NamingStrategy = null;
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,21 +115,39 @@
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                await next();
+            // this will serve up wwwroot
+            app.UseFileServer();
 
-                if (context.Response.StatusCode == 404
-                    && !Path.HasExtension(context.Request.Path.Value))
-                {
-                    context.Request.Path = "/index.html";
-                    await next();
-                }
-            });
-            app.UseStaticFiles();
+            // this will serve up node_modules
+            var provider = new PhysicalFileProvider(
+                Path.Combine(contentRootPath, "node_modules")
+            );
+            var fileServerOptions = new FileServerOptions();
+            fileServerOptions.RequestPath = "/node_modules";
+            fileServerOptions.StaticFileOptions.FileProvider = provider;
+            fileServerOptions.EnableDirectoryBrowsing = true;
+            app.UseFileServer(fileServerOptions);
+
+            //AutoMapperConfiguration.Configure();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+                    {
+                        AutomaticAuthenticate = true,
+                        AutomaticChallenge = true
+                    });
+
+            // Custom authentication middleware
+            //app.UseMiddleware<AuthMiddleware>();
+
+            // Add MVC to the request pipeline.
             app.UseMvc(routes =>
             {
-                routes.MapWebApiRoute("defaultApi", "api/{controller}/{id?}");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             });
 
             DbInitializer.Initialize(app.ApplicationServices);
