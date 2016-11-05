8b790d6f346d3370e200f091c1da92f9bb17013b
VenelinGP <venelin.petkov@netcoms.eu>
2016-11-06 00:04:28 +0200

    Adding core/models

---
+++ EClassBook.API/wwwroot/app/core/models/course.ts
@@ -0,0 +1,16 @@
+﻿export class Course {
+    Id: number;
+    Name: string;
+    UserId: string;
+    TeacherName: string;
+
+    constructor(id: number,
+        name: string,
+        userId: string,
+        user: string) {
+        this.Id = id;
+        this.Name = name;
+        this.UserId = userId;
+        this.TeacherName = user;
+    }
+}
---
+++ EClassBook.API/wwwroot/app/core/models/teacher.ts
@@ -0,0 +1,22 @@
+﻿export class Teacher {
+    Id: number;
+    Username: string;
+    FirstName: string;
+    LastName: string;
+    AddressId: number;
+    RoleID: number;
+
+    constructor(id: number,
+        username: string,
+        firstName: string,
+        lastName: string,
+        addressId: number,
+        roleID: number) {
+        this.Id = id;
+        this.Username = username;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.AddressId = addressId;
+        this.RoleID = roleID;
+    }
+}
